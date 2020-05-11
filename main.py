7a583ea0652c585b6d936a5598f5b0dec966c035
Alser69rus <evdokimov69rus@mail.ru>
2020-05-11 10:34:32 +0300

    добавлена форма с 7 панелями написан тест размеров формы

---
+++ main.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+from ui import form
+
+
+class Main:
+    def __init__(self):
+        print('Стенд А3139')
+        print('(c) ПКБ ЦТ, 2020')
+
+        self.form = form.MainForm()
+        logging.debug('main form load ok')
+
+        self.stm = QtCore.QStateMachine()
+
+    def start(self):
+        self.form.show()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    main = Main()
+    main.start()
+
+    sys.exit(app.exec_())
---
+++ test/test_form_size.py
@@ -0,0 +1,20 @@
+import unittest
+from PyQt5 import QtWidgets, QtTest
+from ui import form
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.app = QtWidgets.QApplication([])
+        self.form = form.MainForm()
+        QtTest.QTest.qWaitForWindowActive(self.form)
+
+    def test_width(self):
+        self.assertEqual(self.form.width(), 1024, msg='form width')
+
+    def test_height(self):
+        self.assertEqual(self.form.height(), 768, msg='form height')
+
+
+if __name__ == '__main__':
+    unittest.main()
