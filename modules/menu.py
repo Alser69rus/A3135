89f90867d0507fa05a982940d1fe98516275a87f
Alser69rus <evdokimov69rus@mail.ru>
2020-07-06 05:50:33 +0300

    подключено меню РД 042

--- modules/menu.py
+++ modules/menu.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import QState, QEvent
 from controller.controller import Controller
 from modules.btp.btp import Btp
+from modules.rd.rd import Rd
 
 ctrl: Controller
 
@@ -12,63 +13,10 @@ class MenuState(QState):
         ctrl = controller
         ctrl.menu.menu['Главное меню'].button['Выход'].clicked.connect(ctrl.form.close)
         self.btp = Btp(controller=ctrl, menu=self)
+        self.rd = Rd(controller=ctrl, menu=self)
 
     def onEntry(self, event: QEvent) -> None:
         ctrl.show_panel('меню')
         ctrl.show_menu('Главное меню')
         ctrl.button_enable('back up down yes')
         ctrl.menu.active = True
-
-#         menu = ctrl.menu
-#
-#         self.main_menu = Menu('Главное меню', self)
-#         self.btp_menu: QState = SubMenu(self.main_menu, menu.main_btp, 'БТП 020', self)
-#         self.rd_menu: QState = SubMenu(self.main_menu, menu.main_rd, 'РД 042', self)
-#         self.ku_menu: QState = SubMenu(self.main_menu, menu.main_ku, 'КУ 215', self)
-#         self.keb_menu: QState = SubMenu(self.main_menu, menu.main_keb, 'КЭБ 208', self)
-#         self.finish = QFinalState(self)
-#         self.setInitialState(self.main_menu)
-#         self.main_menu.addTransition(menu.main_exit.clicked, self.finish)
-#         self.finished.connect(ctrl.form.close)
-#
-#     def onEntry(self, event: QEvent) -> None:
-#         ctrl.show_panel('меню')
-#         ctrl.menu.active = True
-#
-#     def onExit(self, event: QEvent) -> None:
-#         ctrl.menu.active = False
-#
-#
-# class SubMenu(QState):
-#     def __init__(self, main_menu: QState, button: QPushButton, menu_name: str, parent=None):
-#         super().__init__(parent=parent)
-#         self.reset = Reset(parent=self)
-#         self.report_data = Menu('Подготовка к испытанию', parent=self)
-#         self.menu = Menu(menu_name=menu_name, parent=self)
-#         self.finish = QFinalState(self)
-#         self.setInitialState(self.reset)
-#         self.reset.addTransition(self.report_data)
-#         self.report_data.addTransition(ctrl.button['back'].clicked, self.finish)
-#         self.report_data.addTransition(menu.prepare_menu.done.clicked, self.menu)
-#         self.menu.addTransition(ctrl.button['back'].clicked, self.finish)
-#         main_menu.addTransition(button.clicked, self)
-#         self.addTransition(self.finished, main_menu)
-#
-#
-# class Reset(QState):
-#     def onEntry(self, event: QEvent) -> None:
-#         ctrl.menu.reset_prepare()
-#         menu.menu['БТП 020'].reset()
-#         menu.menu['РД 042'].reset()
-#         menu.menu['КУ 215'].reset()
-#         menu.menu['КЭБ 208'].reset()
-#
-#
-# class Menu(QState):
-#     def __init__(self, menu_name: str, parent=None):
-#         super().__init__(parent=parent)
-#         self.menu_name = menu_name
-#
-#     def onEntry(self, event: QEvent) -> None:
-#         ctrl.show_panel('меню')
-#         ctrl.show_menu(self.menu_name)
---
+++ modules/rd/rd.py
@@ -0,0 +1,80 @@
+from PyQt5.QtCore import QState, QEvent, QFinalState
+from controller.controller import Controller
+from modules.btp.data import BtpData
+
+
+ctrl: Controller
+
+
+class Rd(QState):
+    def __init__(self, controller: Controller, menu: QState):
+        super().__init__(parent=controller.stm)
+        global ctrl
+        ctrl = controller
+        self.finish = QFinalState(self)
+        self.addTransition(self.finished, menu)
+        self.reset = Reset(self)
+        self.disable_menu = DisableMenu(self)
+        self.report_data = ReportData(self)
+        self.menu = Menu(self)
+        menu.addTransition(ctrl.menu.menu['Главное меню'].button['РД 042'].clicked, self)
+
+        self.setInitialState(self.reset)
+        self.reset.addTransition(self.disable_menu)
+        self.disable_menu.addTransition(self.report_data)
+        self.report_data.addTransition(ctrl.button['back'].clicked, self.finish)
+        self.report_data.addTransition(ctrl.menu.prepare_menu.done.clicked, self.menu)
+        self.menu.addTransition(ctrl.button['back'].clicked, self.finish)
+
+        # self.prepare = Prepare(controller=ctrl, menu_state=self.menu)
+        # self.auto_breaking = AutoBreaking(controller=ctrl, menu_state=self.menu)
+        # self.kvt_breaking = KvtBreaking(controller=ctrl, menu_state=self.menu)
+        # self.filling = Filling(controller=ctrl, menu_state=self.menu)
+        # self.tightness = Tightness(controller=ctrl, menu_state=self.menu)
+        # self.emptying = Emptying(controller=ctrl, menu_state=self.menu)
+        # self.substitution = Substitution(controller=ctrl, menu_state=self.menu)
+        # self.speed = Speed(controller=ctrl, menu_state=self.menu)
+        # self.ending = Ending(controller=ctrl, menu_state=self.menu)
+        #
+        # self.ending.report.addTransition(ctrl.report.exit.clicked, self.finish)
+        # self.ending.report.addTransition(ctrl.button['back'].clicked, self.finish)
+
+
+class Reset(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl.menu.reset_prepare()
+        ctrl.menu.menu['БТП 020'].reset()
+        ctrl.btp = BtpData()
+
+
+class DisableMenu(QState):
+    def onEntry(self, event: QEvent) -> None:
+        menu = ctrl.menu.menu['БТП 020']
+        buttons = [
+            'торможение автоматическое',
+            'торможение КВТ',
+            'Время наполненя ТЦ',
+            'Герметичность',
+            'Время снижения',
+            'Замещение торможения',
+            'Повышенная скорость',
+            'Завершение',
+        ]
+        for name in buttons:
+            menu.button[name].setEnabled(True)
+
+
+class ReportData(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl.show_menu('Подготовка к испытанию')
+
+
+class Menu(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl.show_menu('БТП 020')
+        ctrl.show_panel('меню')
+        ctrl.button_enable('back up down yes')
+        ctrl.menu.active = True
+
+    def onExit(self, event: QEvent) -> None:
+        ctrl.menu.active = False
