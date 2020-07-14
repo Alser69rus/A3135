e8e5ea1c173f05d260e5f1006eb210e7d4b17ab8
Alser69rus <evdokimov69rus@mail.ru>
2020-07-14 09:48:26 +0300

    Протокол РД 042

--- modules/rd/report.py
+++ modules/rd/report.py
@@ -56,126 +56,6 @@ class Report(QState):
         num = settings.value('protocol/num', 0, int)
         return num, date
 
-    def mm_to_pixel(self, x: float, y: float) -> Tuple[int, int]:
-        k = self.resolution / 25.4
-        return round(x * k), round(y * k)
-
-    def preview(self, printer):
-        painter = self.get_painter(printer)
-        painter.begin(printer)
-        self.header()
-        self.table()
-        self.bottom()
-
-        painter.end()
-
-    def header(self):
-        self.painter.setFont(self.font_hb)
-        num, date = self.get_report_num_and_date()
-        self.pos = (80, 10)
-        self.draw_text(f'Протокол № {num} от {date}')
-        self.painter.setFont(self.font_h)
-        self.pos = (35, 15)
-        self.draw_text(f'испытания реле давления РД 042 для тепловозов ТЭП70')
-        self.draw_text(f'Заводской № {ctrl.report_header.dev_num}             '
-                       f'Дата изготовления: {ctrl.report_header.date}')
-        self.draw_text(f'Локомотив: {ctrl.report_header.locomotive}                '
-                       f'Секция: {ctrl.report_header.section}')
-
-    def bottom(self):
-        self.painter.setFont(self.font_h)
-        self.draw_text('')
-        self.draw_text('')
-        self.draw_text(f'Испытание провел:    __________________ {ctrl.report_header.name_1}')
-        self.draw_text('')
-        self.draw_text(f'Испытание проверил:__________________ {ctrl.report_header.name_2}')
-
-    def table(self):
-        self.pos = (25, 30)
-        self.auto_break()
-        self.kvt_break()
-        self.fill()
-        self.tightness()
-        self.empty()
-        self.substitution()
-        self.speed()
-
-    def substitution(self):
-        self.cell('Проверка работы БТП при замещении электрического тормоза', font=self.font_b)
-        self.cell('Время наполнения ТС, с\n(с 0 до 0,16 МПА)',
-                  'не более 4c',
-                  f'{ctrl.btp.substitution.time_as_text(0)}',
-                  f'{ctrl.btp.substitution.time_as_text(1)}'
-                  , height=2)
-
-    def speed(self):
-        self.cell('Проверка работы БТП при движении локомотива на повышенных скоростях', font=self.font_b)
-        self.cell('Время наполнения ТЦ, с\n(с 0 до 0,56 МПа)',
-                  'не более 4 с',
-                  f'{ctrl.btp.speed_fill.time_as_text(0)}',
-                  f'{ctrl.btp.speed_fill.time_as_text(1)}',
-                  height=2)
-        self.cell('Время снижения давления в ТЦ, с\n(с max до 0 МПа)',
-                  'не более 4 с',
-                  f'{ctrl.btp.speed_empty.time_as_text(0)}',
-                  f'{ctrl.btp.speed_empty.time_as_text(1)}',
-                  height=2)
-        self.cell('Проверка проходимости канала к\n отпускному клапвну',
-                  '',
-                  f'{ctrl.btp.sped_ok[0]}',
-                  f'{ctrl.btp.sped_ok[1]}',
-                  height=2)
-
-    def fill(self):
-        self.cell('Время наполнения ТЦ \nпри управлении КВТ, с\n(с 0 до 0,35 МПа)',
-                  'не более 4 с',
-                  f'{ctrl.btp.fill_time.time_as_text(0)}',
-                  f'{ctrl.btp.fill_time.time_as_text(1)}',
-                  height=3)
-
-    def tightness(self):
-        self.cell('Герметичность мест соединений', '', f'{ctrl.btp.tightness}')
-
-    def empty(self):
-        self.cell('Время снижения давления в ТЦ\nпри управлении КВТ, с\n(с 0,35 до 0,005 МПа)',
-                  'не более 13 с',
-                  f'{ctrl.btp.empty_time.time_as_text(0)}',
-                  f'{ctrl.btp.empty_time.time_as_text(1)}',
-                  height=3)
-
-    def auto_break(self):
-        self.cell(f'Проверка ступенчатого торможения и отпуска при действии автоматического тормоза', font=self.font_b)
-        self.cell('Ступень ВР', 'Норма, МПа', 'ТЦ1, МПа', 'ТЦ2, МПа')
-        self.cell('Торможение')
-        for i in range(4):
-            self.cell(f'{ctrl.btp.auto_breaking.position_as_text(i)}',
-                      f'{ctrl.btp.auto_breaking.range_as_text(i)}',
-                      f'{ctrl.btp.auto_breaking.tc_as_text(0, i)}',
-                      f'{ctrl.btp.auto_breaking.tc_as_text(1, i)}')
-        self.cell('Отпуск')
-        for i in range(4, 8):
-            self.cell(f'{ctrl.btp.auto_breaking.position_as_text(i)}',
-                      f'{ctrl.btp.auto_breaking.range_as_text(i)}',
-                      f'{ctrl.btp.auto_breaking.tc_as_text(0, i)}',
-                      f'{ctrl.btp.auto_breaking.tc_as_text(1, i)}')
-
-    def kvt_break(self):
-        self.cell(f'Проверка ступенчатого торможения и отпуска при управлении краном\nвспомогательного тормоза',
-                  font=self.font_b, height=2)
-        self.cell('Ступень ВР', 'Норма, МПа', 'ТЦ1, МПа', 'ТЦ2, МПа')
-        self.cell('Торможение')
-        for i in range(4):
-            self.cell(f'{ctrl.btp.kvt_breaking.position_as_text(i)}',
-                      f'{ctrl.btp.kvt_breaking.range_as_text(i)}',
-                      f'{ctrl.btp.kvt_breaking.tc_as_text(0, i)}',
-                      f'{ctrl.btp.kvt_breaking.tc_as_text(1, i)}')
-        self.cell('Отпуск')
-        for i in range(4, 8):
-            self.cell(f'{ctrl.btp.kvt_breaking.position_as_text(i)}',
-                      f'{ctrl.btp.kvt_breaking.range_as_text(i)}',
-                      f'{ctrl.btp.kvt_breaking.tc_as_text(0, i)}',
-                      f'{ctrl.btp.kvt_breaking.tc_as_text(1, i)}')
-
     def draw_text(self, text: str) -> None:
         x, y = self.mm_to_pixel(*self.pos)
         self.painter.drawText(x, y, text)
@@ -197,7 +77,7 @@ class Report(QState):
         if col == 1:
             painter.drawText(x1, y1, w, h, Qt.AlignCenter, args[0])
         else:
-            tab = (90, 120, 160, 200)
+            tab = (120, 160, 200)
             for i, text in enumerate(args):
                 x2, y2 = self.mm_to_pixel(tab[i], y)
                 w, h = x2 - x1, y2 - y1
@@ -205,3 +85,73 @@ class Report(QState):
                 painter.drawText(x1, y1, w, h, Qt.AlignCenter, text)
                 x1 = x2
         self.pos = (self.pos[0], y)
+
+    def mm_to_pixel(self, x: float, y: float) -> Tuple[int, int]:
+        k = self.resolution / 25.4
+        return round(x * k), round(y * k)
+
+    def preview(self, printer):
+        painter = self.get_painter(printer)
+        painter.begin(printer)
+        self.header()
+        self.table()
+        self.bottom()
+        painter.end()
+
+    def header(self):
+        self.painter.setFont(self.font_hb)
+        num, date = self.get_report_num_and_date()
+        self.pos = (80, 10)
+        self.draw_text(f'Протокол № {num} от {date}')
+        self.painter.setFont(self.font_h)
+        self.pos = (55, 15)
+        self.draw_text(f'испытания реле давления РД 042 для тепловозов ТЭП70')
+        self.draw_text(f'Заводской № {ctrl.report_header.dev_num}             '
+                       f'Дата изготовления: {ctrl.report_header.date}')
+        self.draw_text(f'Локомотив: {ctrl.report_header.locomotive}                '
+                       f'Секция: {ctrl.report_header.section}')
+        self.pos = (25, 30)
+        self.cell('Наименование', 'Норма', 'Факт ТЦ2', font=self.font_b)
+
+    def bottom(self):
+        self.painter.setFont(self.font_h)
+        self.draw_text('')
+        self.draw_text('')
+        self.draw_text(f'Испытание провел:    __________________ {ctrl.report_header.name_1}')
+        self.draw_text('')
+        self.draw_text(f'Испытание проверил:__________________ {ctrl.report_header.name_2}')
+
+    def table(self):
+
+        self.fill()
+        self.sensitivity()
+        self.empty()
+        self.valve()
+        self.junctions()
+
+    def fill(self):
+        self.cell('Время наполнения ТЦ, с', 'не более 4', f'{ctrl.rd.fill.text()}')
+
+    def sensitivity(self):
+        self.cell('Автоматическое поддержание зарядного\n'
+                  'давления при утечке (чувствительность), МПа',
+                  'не более 0,015',
+                  f'{ctrl.rd.sensitivity.text()}',
+                  height=2
+                  )
+
+    def empty(self):
+        self.cell('Время снижения давления в ТЦ, с',
+                  'не более 10',
+                  f'{ctrl.rd.empty.text()}'
+                  )
+
+    def valve(self):
+        self.cell('Плотность мест соединений',
+                  '',
+                  f'{ctrl.rd.junctions}')
+
+    def junctions(self):
+        self.cell('Герметичность атмосферного клапана',
+                  '',
+                  f'{ctrl.rd.valve}')
