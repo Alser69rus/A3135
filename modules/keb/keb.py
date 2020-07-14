7da535a5be07d1f1ddb70571af7d0f80bb5e1a84
Alser69rus <evdokimov69rus@mail.ru>
2020-07-14 13:57:32 +0300

    подключено меню КЭБ 208

---
+++ modules/keb/keb.py
@@ -0,0 +1,82 @@
+from PyQt5.QtCore import QState, QEvent, QFinalState
+from controller.controller import Controller
+from modules.keb.data import KebData
+
+
+# from modules.rd.prepare import Prepare
+# from modules.rd.fill import Fill
+# from modules.rd.sensitivity import Sensitivity
+# from modules.rd.empty import Empty
+# from modules.rd.valve import Valve
+# from modules.rd.junctions import Junctions
+# from modules.rd.end import End
+
+
+class Keb(QState):
+    def __init__(self, parent):
+        super().__init__(parent=parent.controller.stm)
+        self.controller: Controller = parent.controller
+        ctrl = self.controller
+        self.finish = QFinalState(self)
+        self.addTransition(self.finished, parent)
+        self.reset = Reset(self)
+        self.disable_menu = DisableMenu(self)
+        self.report_data = ReportData(self)
+        self.menu = Menu(self)
+        parent.addTransition(ctrl.menu.menu['Главное меню'].button['КЭБ 208'].clicked, self)
+
+        self.setInitialState(self.reset)
+        self.reset.addTransition(self.disable_menu)
+        self.disable_menu.addTransition(self.report_data)
+        self.report_data.addTransition(ctrl.button['back'].clicked, self.finish)
+        self.report_data.addTransition(ctrl.menu.prepare_menu.done.clicked, self.menu)
+        self.menu.addTransition(ctrl.button['back'].clicked, self.finish)
+
+        # self.prepare = Prepare(self)
+        # self.fill = Fill(self)
+        # self.sensitivity = Sensitivity(self)
+        # self.empty = Empty(self)
+        # self.valve = Valve(self)
+        # self.junctions = Junctions(self)
+        # self.end = End(self)
+
+
+class Reset(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl: Controller = self.parent().controller
+        ctrl.menu.reset_prepare()
+        ctrl.menu.menu['КЭБ 208'].reset()
+        ctrl.keb = KebData()
+
+
+class DisableMenu(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl: Controller = self.parent().controller
+        menu = ctrl.menu.menu['КЭБ 208']
+        buttons = [
+            'Время торможения',
+            'Герметичность соединений',
+            'Время отпуска',
+            'Завершение',
+        ]
+        for name in buttons:
+            menu.button[name].setEnabled(False)
+
+
+class ReportData(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl: Controller = self.parent().controller
+        ctrl.show_menu('Подготовка к испытанию')
+
+
+class Menu(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl: Controller = self.parent().controller
+        ctrl.show_menu('КЭБ 208')
+        ctrl.show_panel('меню')
+        ctrl.show_button('back up down yes')
+        ctrl.menu.active = True
+
+    def onExit(self, event: QEvent) -> None:
+        ctrl: Controller = self.parent().controller
+        ctrl.menu.active = False
--- modules/menu.py
+++ modules/menu.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import QState, QEvent
 from controller.controller import Controller
 from modules.btp.btp import Btp
 from modules.rd.rd import Rd
+from modules.keb.keb import Keb
 
 ctrl: Controller
 
@@ -10,11 +11,12 @@ class MenuState(QState):
     def __init__(self, controller: Controller):
         super().__init__(parent=controller.stm)
         global ctrl
-        self.controller:Controller=controller
+        self.controller: Controller = controller
         ctrl = self.controller
         ctrl.menu.menu['Главное меню'].button['Выход'].clicked.connect(ctrl.form.close)
         self.btp = Btp(controller=ctrl, menu=self)
         self.rd = Rd(self)
+        self.keb = Keb(self)
 
     def onEntry(self, event: QEvent) -> None:
         ctrl.show_panel('меню')
