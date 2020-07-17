76d0cbe9c8aa78047631d3390ae56f33a08d0715
Alser69rus <evdokimov69rus@mail.ru>
2020-07-17 15:46:48 +0300

    data KU

---
+++ modules/ku/data.py
@@ -0,0 +1,110 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass()
+class Fill:
+    t1 = datetime.now()
+    t2 = datetime.now()
+    empty_value = True
+
+    def reset(self):
+        self.empty_value = True
+
+    def start(self):
+        self.t1 = datetime.now()
+        self.t2 = datetime.now()
+
+    def stop(self):
+        self.t2 = datetime.now()
+        self.empty_value = False
+
+    def time(self) -> float:
+        return (self.t2 - self.t1).total_seconds()
+
+    def text(self) -> str:
+        if self.empty_value:
+            return '-'
+        if self.result():
+            return f'{self.time():.1f}'
+        return f'{self.time():.1f} (не норма)'
+
+    def result(self) -> bool:
+        return 0 < self.time() <= 3
+
+
+@dataclass()
+class Sensitivity:
+    ptc = []
+    t1 = datetime.now()
+    t2 = datetime.now()
+    empty_value = True
+
+    def reset(self):
+        self.empty_value = True
+
+    def start(self):
+        self.t1 = datetime.now()
+        self.t2 = datetime.now()
+        self.ptc = []
+        self.empty_value = False
+
+    def update(self, value: float):
+        self.t2 = datetime.now()
+        self.ptc.append(value)
+
+    def time(self) -> float:
+        return (self.t2 - self.t1).total_seconds()
+
+    def delta_p(self) -> float:
+        return max(self.ptc) - min(self.ptc)
+
+    def result(self) -> bool:
+        return self.delta_p() <= 0.015
+
+    def text(self) -> str:
+        if self.empty_value:
+            return '-'
+        if self.result():
+            return f'{self.delta_p():.3f}'
+        return f'{self.delta_p():.3f} (не норма)'
+
+
+@dataclass()
+class Empty:
+    t1 = datetime.now()
+    t2 = datetime.now()
+    empty_value = True
+
+    def reset(self):
+        self.empty_value = True
+
+    def start(self):
+        self.t1 = datetime.now()
+        self.t2 = datetime.now()
+        self.empty_value = False
+
+    def update(self):
+        self.t2 = datetime.now()
+
+    def time(self) -> float:
+        return (self.t2 - self.t1).total_seconds()
+
+    def text(self) -> str:
+        if self.empty_value:
+            return '-'
+        if self.result():
+            return f'{self.time():.1f}'
+        return f'{self.time():.1f} (не норма)'
+
+    def result(self) -> bool:
+        return 0 < self.time() <= 10
+
+
+@dataclass()
+class RdData:
+    fill = Fill()
+    sensitivity = Sensitivity()
+    empty = Empty()
+    valve = '-'
+    junctions = '-'
