0d2c1361ac713af72bdd5bb5018c01785cf5abe6
Alser69rus <evdokimov69rus@mail.ru>
2020-07-24 10:20:04 +0300

    подготовка ку - введена проверка утечки 05 и резервуара в сброс

--- modules/ku/prepare.py
+++ modules/ku/prepare.py
@@ -22,12 +22,16 @@ class Prepare(QState):
         self.start = Start(self)
         self.install = Install(self)
         self.prepare_pressure = common.prepare_pressure(self)
+        self.leak = Leak(self)
+        self.tank = Tank(self)
         self.enable_menu = EnableMenu(self)
 
         self.setInitialState(self.start)
         self.start.addTransition(self.install)
         self.install.addTransition(ctrl.button['yes'].clicked, self.prepare_pressure)
-        self.prepare_pressure.addTransition(self.prepare_pressure.finished, self.enable_menu)
+        self.prepare_pressure.addTransition(self.prepare_pressure.finished, self.leak)
+        self.leak.addTransition(ctrl.switch['leak 0,5'].low_value, self.tank)
+        self.tank.addTransition(ctrl.switch_with_neutral['tank'].state_two, self.enable_menu)
 
 
 class Start(QState):
@@ -42,6 +46,16 @@ class Install(QState):
         ctrl.setText(f'<p>Установите КУ 215 на прижимю</p>'
                      f'<p>Откройте кран на атмосферной трубе, отходящей от плиты прижима КУ 215.</p>'
                      f'<p><br>Для продолжения нажмите "ДА".</p>')
+
+
+class Leak(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl.setText('Отключите тумблер "УТЕЧКА ø0,5".')
+
+
+class Tank(QState):
+    def onEntry(self, event: QEvent) -> None:
+        ctrl.setText('Включите тумблер "НАКОП. РЕЗ." в положение "СБРОС"')
 
 
 class EnableMenu(QFinalState):
